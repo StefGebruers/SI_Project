ef5b8bc5b64c707642164b7da1ddcbdcf5f19c2e
SorenNotredame <r0982640@student.thomasmore.be>
2025-02-26 09:34:45 +0100

    Edited sample code read p1

--- read_p1.py
+++ read_p1.py
@@ -2,15 +2,20 @@
 
 # This script will read data from serial connected to the digital meter P1 port
 
-# Created by Jens Depuydt
-# https://www.jensd.be
-# https://github.com/jensdepuydt
+# credits sample code
+    # Jens Depuydt
+    # https://www.jensd.be
+    # https://github.com/jensdepuydt
 
 import serial
 import sys
 import crcmod.predefined
 import re
 from tabulate import tabulate
+import os
+
+# Create clear function to clear the terminal
+clear = lambda: os.system('clear')
 
 # Change your serial port here:
 serialport = '/dev/ttyUSB0'
@@ -20,31 +25,17 @@ debug = False
 
 # Add/update OBIS codes here:
 obiscodes = {
-    "0-0:1.0.0": "Timestamp",
-    "0-0:96.3.10": "Switch electricity",
-    "0-1:24.4.0": "Switch gas",
-    "0-0:96.1.1": "Meter serial electricity",
-    "0-1:96.1.1": "Meter serial gas",
     "0-0:96.14.0": "Current rate (1=day,2=night)",
     "1-0:1.8.1": "Rate 1 (day) - total consumption",
     "1-0:1.8.2": "Rate 2 (night) - total consumption",
     "1-0:2.8.1": "Rate 1 (day) - total production",
     "1-0:2.8.2": "Rate 2 (night) - total production",
     "1-0:21.7.0": "L1 consumption",
-    "1-0:41.7.0": "L2 consumption",
-    "1-0:61.7.0": "L3 consumption",
     "1-0:1.7.0": "All phases consumption",
     "1-0:22.7.0": "L1 production",
-    "1-0:42.7.0": "L2 production",
-    "1-0:62.7.0": "L3 production",
     "1-0:2.7.0": "All phases production",
     "1-0:32.7.0": "L1 voltage",
-    "1-0:52.7.0": "L2 voltage",
-    "1-0:72.7.0": "L3 voltage",
     "1-0:31.7.0": "L1 current",
-    "1-0:51.7.0": "L2 current",
-    "1-0:71.7.0": "L3 current",
-    "0-1:24.2.3": "Gas consumption"
     }
 
 
@@ -83,22 +74,11 @@ def parsetelegramline(p1line):
         # format:OBIS(value), gas: OBIS(timestamp)(value)
         values = re.findall(r'\(.*?\)', p1line)
         value = values[0][1:-1]
-        # timestamp requires removal of last char
-        if obis == "0-0:1.0.0" or len(values) > 1:
-            value = value[:-1]
-        # report of connected gas-meter...
-        if len(values) > 1:
-            timestamp = value
-            value = values[1][1:-1]
-        # serial numbers need different parsing: (hex to ascii)
-        if "96.1.1" in obis:
-            value = bytearray.fromhex(value).decode()
-        else:
-            # separate value and unit (format:value*unit)
-            lvalue = value.split("*")
-            value = float(lvalue[0])
-            if len(lvalue) > 1:
-                unit = lvalue[1]
+        # separate value and unit (format:value*unit)
+        lvalue = value.split("*")
+        value = float(lvalue[0])
+        if len(lvalue) > 1:
+            unit = lvalue[1]
         # return result in tuple: description,value,unit,timestamp
         if debug:
             print (f"description:{obiscodes[obis]}, \
@@ -124,7 +104,9 @@ def main():
                 if debug:
                     print ("Found beginning of P1 telegram")
                 p1telegram = bytearray()
-                print('*' * 60 + "\n")
+                #print('*' * 60 + "\n")
+                clear()
+                
             # add line to complete telegram
             p1telegram.extend(p1line)
             # P1 telegram ends with ! + CRC16 checksum
